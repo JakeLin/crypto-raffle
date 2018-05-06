68abdb17c2c067a0b7e648d34396e57a70d72adf
Jake Lin <jakelinau@gmail.com>
2018-05-06 10:55:29 +1000

    Use arrow function

--- test/Raffle.js
+++ test/Raffle.js
@@ -1,7 +1,7 @@
 const Raffle = artifacts.require('./Raffle.sol');
 
-contract('Raffle', async function(accounts) {
-    it('the owner should be the first account', async function() {
+contract('Raffle', async (accounts) => {
+    it('the owner should be the first account', async () => {
         let raffle = await Raffle.deployed();
         let owner = await raffle.owner.call();
         assert.equal(owner, accounts[0]);
